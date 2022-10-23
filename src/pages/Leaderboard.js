5452771e9232a0555053036049a800d803f4788d
smart8099 <abdulbasitm810@gmail.com>
2022-10-23 08:57:07 -0700

    Add Home page and Create wireframe of Record Management System

---
+++ src/pages/Leaderboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Leaderboard = () => (
+
+  <div className="under--construction">
+    <h1>Toolhub Record Management  Leaderboard</h1>
+  </div>
+);
+
+export default Leaderboard;
---
+++ src/pages/RecordsBoard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RecordsBoard = () => (
+
+  <div className="under--construction">
+    <h1>RecordsBoard Under Construction..</h1>
+  </div>
+);
+
+export default RecordsBoard;
