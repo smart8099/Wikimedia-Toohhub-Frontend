37053541ed2f3ab2d3af7e7980ef8c13929b0ee4
smart8099 <abdulbasitm810@gmail.com>
2022-10-24 22:03:12 -0700

    Correct import issue

--- src/pages/RecordsBoard.js
+++ src/pages/RecordsBoard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Bar } from "react-chartjs-2";
-import Chart from 'chart.js/auto';
+import Chart from 'chart.js/auto'; //eslint-disable-line
 
 const RecordsBoard = () => (
 
